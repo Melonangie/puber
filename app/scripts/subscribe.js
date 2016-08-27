6fd1a737973d709a723b8f220a6457b4a10a95fd
Maria Rabelero <m.rabelero.campos@accenture.com>
2016-08-27 05:33:58 -0500

    added dist folder

--- app/scripts/subscribe.js
+++ app/scripts/subscribe.js
@@ -13,7 +13,6 @@ $(function() {
             var name = $('input#name').val();
             var email = $('input#email').val();
             var phone = $('input#phone').val();
-            var message = $('textarea#message').val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -32,7 +31,7 @@ $(function() {
                     $('#success > .alert-success').html('<button type=\'button\' class=\'close\' data-dismiss=\'alert\' aria-hidden=\'true\'>&times;')
                         .append('</button>');
                     $('#success > .alert-success')
-                        .append('<strong>Your message has been sent. </strong>');
+                        .append('<strong>Thanks you will receive a message in short to validate your account.</strong>');
                     $('#success > .alert-success')
                         .append('</div>');
                     //clear all fields
@@ -43,7 +42,7 @@ $(function() {
                     $('#success').html('<div class=\'alert alert-danger\'>');
                     $('#success > .alert-danger').html('<button type=\'button\' class=\'close\' data-dismiss=\'alert\' aria-hidden=\'true\'>&times;')
                         .append('</button>');
-                    $('#success > .alert-danger').append('<strong>Sorry ' + firstName + ', it seems that my mail server is not responding. Please try again later!');
+                    $('#success > .alert-danger').append('<strong>Sorry ' + firstName + ', we\'re having difficulties at the moment. Please try again later!');
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger('reset');
